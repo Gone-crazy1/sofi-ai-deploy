7a3df484851da858cc25bbce74ffc3519bc22fae
Gone-crazy1 <mrhawt10@yahoo.com>
2025-06-07 19:43:49 -0400

    Fix CSRF error on Telegram webhook

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ import json
 from datetime import datetime
 import hashlib
 import hmac
+from flask_wtf.csrf import CSRFError
 
 # Load environment variables from .env at the very top
 from dotenv import load_dotenv
